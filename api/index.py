4df86cd6e5f4a4145bfaf0786329af04e90780bd
Jan Drmota <jandrmota@Jans-MacBook-Air.local>
2024-12-01 11:16:40 +0000

    updated

--- api/index.py
+++ api/index.py
@@ -1,8 +1,60 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
+import pandas as pd
+import os
+from scipy.io import arff
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+from joblib import Parallel, delayed 
+from joblib import load
+
+import json
+
+# Get the absolute path of the current directory
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the full path to the model
+model_path = os.path.join(base_dir, "models", "vertispine3CML.pkl")
+
+# Load the model
+model = load(model_path)
+
+
+
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#data_file = os.path.join(basedir, 'models/column_3C_weka.arff')
+
 app = Flask(__name__)
 
-@app.route("/api/python", methods=['GET'])
+@app.route("/api/python") #, methods=['GET']
 def hello_world():
-    username = request.args.get('username')
-    return {"message": username}
+    
+    d = {'pelvic_incidence': [], 'pelvic_tilt': [], 'lumbar_lordosis_angle': [], 'sacral_slope': [], 'pelvic_radius': [], 'degree_spondylolisthesis': []}
+    
+    d['pelvic_incidence'] = [float(request.args.get('pelvic_incidence'))];
+    d['pelvic_tilt'] = [float(request.args.get('pelvic_tilt'))];
+    d['lumbar_lordosis_angle'] = [float(request.args.get('lumbar_lordosis_angle'))];
+    d['sacral_slope'] = [float(request.args.get('sacral_slope'))];
+    d['pelvic_radius'] = [float(request.args.get('pelvic_radius'))];
+    d['degree_spondylolisthesis'] = [float(request.args.get('degree_spondylolisthesis'))];
+    
+    #d = {'pelvic_incidence': pelvic_incidence, 'pelvic_tilt': pelvic_tilt, 'lumbar_lordosis_angle': lumbar_lordosis_angle, 'sacral_slope': sacral_slope, 'pelvic_radius': pelvic_radius, 'degree_spondylolisthesis': degree_spondylolisthesis}
+    #d = {'pelvic_incidence': [53.854798, 74.433593, 70.952728], 'pelvic_tilt': [19.230643, 41.557331, 20.159931], 'lumbar_lordosis_angle': [32.779060, 27.700000, 62.859109], 'sacral_slope': [34.624155, 32.876262, 50.792797], 'pelvic_radius': [121.670915, 107.949304, 116.177932], 'degree_spondylolisthesis': [5.329843, 5.000089, 32.522331]}
+    df = pd.DataFrame(data=d)
+    prediction = model.predict(df)
+    prediction_list = prediction.tolist()
+    #knn_from_joblib = joblib.load(data_file) 
+    #username = str(knn_from_joblib.predict(X_test))
+    #arff_file = arff.loadarff(data_file)
+    #df = pd.DataFrame(arff_file[0])
+    #username = str(df.shape[0])
+    
+    
+    #return {"pelvic_incidence": pelvic_incidence, "pelvic_tilt": pelvic_tilt, "lumbar_lordosis_angle": lumbar_lordosis_angle, "sacral_slope": sacral_slope, "pelvic_radius": pelvic_radius, "degree_spondylolisthesis": degree_spondylolisthesis}
+    return jsonify({'prediction': prediction_list})
--- app/page.tsx
+++ app/page.tsx
@@ -6,133 +6,210 @@ import Link from 'next/link'
 
 export default function Home() {
 
-  const [value, setValue] = useState("First");
+  const [value, setValue] = useState("Hi");
+  const [loading, setLoading] = useState(false);
+  const [showOutput, setShowOutput] = useState(false);
 
-  useEffect(()=> {
-    fetch('/api/python?username=Second').then(response => {
-    if(response.ok){
-        //console.log(response)
-      return response.json()
-    }
-    }).then(data => setValue(data.message))/*.then(data => setAllActivities(data))
+  const [pelvic_incidenceTXT, setpelvic_incidenceTXT] = useState("");
+  const [pelvic_tiltTXT, setpelvic_tiltTXT] = useState("");
+  const [lumbar_lordosis_angleTXT, setlumbar_lordosis_angleTXT] = useState("");
+  const [sacral_slopeTXT, setsacral_slopeTXT] = useState("");
+  const [pelvic_radiusTXT, setpelvic_radiusTXT] = useState("");
+  const [degree_spondylolisthesisTXT, setdegree_spondylolisthesisTXT] = useState("");
+
+  const handleSubmit = (event) => {
+    setLoading(true);
+    setShowOutput(false);
+    event.preventDefault()
+
+    const pelvic_incidence = event.target.elements.pelvic_incidence.value;
+    const pelvic_tilt = event.target.elements.pelvic_tilt.value;
+    const lumbar_lordosis_angle = event.target.elements.lumbar_lordosis_angle.value;
+    const sacral_slope = event.target.elements.sacral_slope.value;
+    const pelvic_radius = event.target.elements.pelvic_radius.value;
+    const degree_spondylolisthesis = event.target.elements.degree_spondylolisthesis.value;
+
+    setpelvic_incidenceTXT(pelvic_incidence);
+    setpelvic_tiltTXT(pelvic_tilt);
+    setlumbar_lordosis_angleTXT(lumbar_lordosis_angle);
+    setsacral_slopeTXT(sacral_slope);
+    setpelvic_radiusTXT(pelvic_radius);
+    setdegree_spondylolisthesisTXT(degree_spondylolisthesis);
+
+    const fetUrl = '/api/python?pelvic_incidence=' + encodeURIComponent(pelvic_incidence) + '&pelvic_tilt=' + encodeURIComponent(pelvic_tilt) + '&lumbar_lordosis_angle=' + encodeURIComponent(lumbar_lordosis_angle) + '&sacral_slope=' + encodeURIComponent(sacral_slope) + '&pelvic_radius=' + encodeURIComponent(pelvic_radius) + '&degree_spondylolisthesis=' + encodeURIComponent(degree_spondylolisthesis);
+
+    console.log(fetUrl);
+
+    fetch(fetUrl).then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      if (response.ok) {
+
+        return response.json()
+      }
+    }).then(data => {
+      setValue(data.prediction)
+      setLoading(false);
+      setShowOutput(true);
+    })/*.then(data => setAllActivities(data))
         .then(data => {
             console.log(data);
             //var jsonData = JSON.parse((data));
             //console.log(jsonData[0].name);
         })*/
 
-},[])
+  }
+
+  useEffect(() => {
+
+
+  }, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
+        <p className="items-center fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <Image
+            src="/spine.png"
+            width={40}
+            height={40}
+            alt="Picture of the author"
+            draggable={false}
+          />
+          <code className="font-mono font-bold">vertispine</code>
+        </p>
+        <p className="my-5">by:
+          <Link href="https://www.linkedin.com/in/jandrmota/" target="_blank">
+            <code className="font-mono font-bold mx-2">Jan Drmota</code>
           </Link>
         </p>
-        <p>{value}</p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+
+      <div className="relative place-items-center w-full max-w-5xl m-10">
+        <div>
+          <p className="my-5">This is a simulated application illustrating the use of a 3 class machine learning classification algorithm in the orthopaedics outpatient clinical setting, submitted to the BOA x Stryker AI in Orthopaedics Hackathon 2024.</p>
+          <div className="flex items-center justify-center">
+            <div className="cursor-pointer justify-center rounded bg-zinc-800/30 p-3 m-3">
+              <Link href="/vertispine.pdf" target="_blank" rel='noopener noreferrer'>
+                <code>⬇️ Download full detail PDF</code>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="bg-gray-500 border-gray-600 rounded p-3 m-3">
+          <div className="flex flex-row">
+            <div className="w-10 h-10 rounded-full flex justify-center items-center bg-white">
+              <p className="text-black">1</p>
+            </div>
+            <p className="m-2">Selecting patient</p>
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            <div className="my-3">
+              <label htmlFor="pt_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient name:</label>
+              <input disabled type="text" id="pt_name" className="cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe &#10003;" />
+            </div>
+            <div className="my-3">
+              <label htmlFor="pt_id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patient ID:</label>
+              <input disabled type="number" id="pt_id" className="cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1234567 &#10003;" />
+            </div>
+          </div>
+          <p className="text-sm my-2 italic">This would be connected to EPR system.</p>
+        </div>
+        <div className="bg-gray-500 border-gray-600 rounded p-3 m-3">
+          <div className="flex flex-row">
+            <div className="w-10 h-10 rounded-full flex justify-center items-center bg-white">
+              <p className="text-black">2</p>
+            </div>
+            <p className="m-2">Retrieving data from images</p>
+          </div>
+          <div className="flex flex-row gap-5 m-5 items-center">
+            <Image
+              src="/l1.png"
+              width={50}
+              height={50}
+              alt="Picture of the author"
+              draggable={false}
+            />
+            <Image
+              src="/l2.jpeg"
+              width={50}
+              height={50}
+              alt="Picture of the author"
+              draggable={false}
+            />
+            <p className="text-gray-400">&#10003;</p>
+            <button disabled className="cursor-not-allowed text-white bg-blue-700 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-700 dark:focus:ring-blue-700">Select images</button>
+          </div>
+          <p className="text-sm my-2 italic">This would allow selection of images from PACS and an image processing algorithm would retrieve the data with clinician correction if necessary.</p>
+        </div>
+        <div className="bg-gray-500 border-gray-600 rounded p-3 m-3">
+          <div className="flex flex-row">
+            <div className="w-10 h-10 rounded-full flex justify-center items-center bg-white">
+              <p className="text-black">3</p>
+            </div>
+            <p className="m-2">Analysing data</p>
+          </div>
+          <p className="text-sm my-2 italic">For the simulation, please input data you want the machine learning algorithm to predict. This would be auto-populated from the above.</p>
+        </div>
+        <div className="mx-3">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label htmlFor="pelvic_incidence" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pelvic incidence</label>
+              <input type="number" step="0.000001" id="pelvic_incidence" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="##.######" required />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="pelvic_tilt" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pelvic tilt</label>
+              <input type="number" step="0.000001" id="pelvic_tilt" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="##.######" required />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="lumbar_lordosis_angle" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lumbar lordosis angle</label>
+              <input type="number" step="0.000001" id="lumbar_lordosis_angle" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="##.######" required />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="sacral_slope" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sacral slope</label>
+              <input type="number" step="0.000001" id="sacral_slope" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="##.######" required />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="pelvic_radius" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pelvic radius</label>
+              <input type="number" step="0.000001" id="pelvic_radius" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="###.######" required />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="degree_spondylolisthesis" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Degree spondylolisthesis</label>
+              <input type="number" step="0.000001" id="degree_spondylolisthesis" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="##.######" required />
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Predict</button>
+          </form>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : <></>}
+        {showOutput ? (
+          <div className="bg-green-700 border-gray-600 rounded p-5 m-3">
+            <p className="text-3xl my-3 font-extrabold">{value}</p>
+            <div className="bg-gray-500 border-gray-600 rounded p-3 m-3">
+              <code>
+                <p className="font-bold">For input values:</p>
+                <p className="text-sm">Pelvic incidence: {pelvic_incidenceTXT}</p>
+                <p className="text-sm">Pelvic tilt: {pelvic_tiltTXT}</p>
+                <p className="text-sm">Lumbar lordosis: {lumbar_lordosis_angleTXT}</p>
+                <p className="text-sm">Sacral slope: {sacral_slopeTXT}</p>
+                <p className="text-sm">Pelvic radius: {pelvic_radiusTXT}</p>
+                <p className="text-sm">Degree spondylolisthesis: {degree_spondylolisthesisTXT}</p>
+              </code>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <p className="my-2">Algorithm accuracy: </p>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div >
+    </main >
   )
 }
