d9fde61b294df255fa2e879df8f5c9339f2ec1ab
Jan Drmota <jandrmota@Jans-MacBook-Air.local>
2024-12-01 23:55:28 +0000

    fixed

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 @app.route("/api/python") #, methods=['GET']
 def hello_world():
     
--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     setpelvic_radiusTXT(pelvic_radius);
     setdegree_spondylolisthesisTXT(degree_spondylolisthesis);
 
-    const fetUrl = 'http://159.65.61.32/?pelvic_incidence=' + encodeURIComponent(pelvic_incidence) + '&pelvic_tilt=' + encodeURIComponent(pelvic_tilt) + '&lumbar_lordosis_angle=' + encodeURIComponent(lumbar_lordosis_angle) + '&sacral_slope=' + encodeURIComponent(sacral_slope) + '&pelvic_radius=' + encodeURIComponent(pelvic_radius) + '&degree_spondylolisthesis=' + encodeURIComponent(degree_spondylolisthesis);
+    const fetUrl = 'https://vertispine.me/?pelvic_incidence=' + encodeURIComponent(pelvic_incidence) + '&pelvic_tilt=' + encodeURIComponent(pelvic_tilt) + '&lumbar_lordosis_angle=' + encodeURIComponent(lumbar_lordosis_angle) + '&sacral_slope=' + encodeURIComponent(sacral_slope) + '&pelvic_radius=' + encodeURIComponent(pelvic_radius) + '&degree_spondylolisthesis=' + encodeURIComponent(degree_spondylolisthesis);
 
     /*/api/python? --- commented out in next.config.ts*/
     console.log(fetUrl);
